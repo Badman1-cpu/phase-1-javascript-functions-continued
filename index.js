0c176c7f83908ecbc2d6cc74a73bc2c77b0173bb
Tayler <taylerblevins@gmail.com>
2023-11-12 19:23:53 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(a = 'roller-skate'){
+    return `This Saturday, I want to ${a}!`
+}
+function mondayWork(b = 'go to the office'){
+    return `This Monday, I will ${b}.`
+}
+function wrapAdjective(c='*'){
+    const innerFunction = function(d ='special'){
+        return `You are ${c}${d}${c}!`
+    }
+    return innerFunction
+}
